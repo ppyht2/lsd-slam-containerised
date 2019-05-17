ca380402e69be394c41d2143c4b74414de51de32
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-05-17 08:15:32 +0000

    Search include headers in only unix include directories

--- cmake/modules/FindG2O.cmake
+++ cmake/modules/FindG2O.cmake
@@ -1,17 +1,8 @@
 # Find the header files
 
 FIND_PATH(G2O_INCLUDE_DIR g2o/core/base_vertex.h
-  ${G2O_ROOT}/include
-  ${G2O_ROOT}
-  $ENV{G2O_ROOT}/include
-  $ENV{G2O_ROOT}
-  ${G2O_ROOT}
   /usr/local/include
   /usr/include
-  /opt/local/include
-  /sw/local/include
-  /sw/include
-  NO_DEFAULT_PATH
   )
 
 # Macro to unify finding both the debug and release versions of the
