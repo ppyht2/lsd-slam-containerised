167ca2c26f2f1766b6054a5671568802a1e843c7
Takeshi Ishita <takeshi.ishita@dena.jp>
2019-05-20 20:11:28 +0900

    Install zip before downloading the dataset

--- scripts/download_tum_mono.sh
+++ scripts/download_tum_mono.sh
@@ -1,4 +1,7 @@
 SEQUENCE=30
+
+apt install wget zip  # Assume this is on a docker container
+
 wget -c http://vision.in.tum.de/mono/dataset/sequence_${SEQUENCE}.zip -P data
 unzip -o data/sequence_${SEQUENCE}.zip -d data
 unzip -o data/sequence_${SEQUENCE}/images.zip -d data/sequence_${SEQUENCE}/images/
